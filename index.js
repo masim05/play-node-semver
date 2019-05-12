41fe65d7e30a40947fc265b74566c4de571d22cf
Max Ryndin <rgmasim@gmail.com>
2019-05-12 22:40:02 +0300

    Refactor code.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Octokit = require('@octokit/rest')
 const semver = require('semver')
 
 const list  = [
@@ -26,4 +27,55 @@ const sorted = semver.sort(list)
   'v1.1.1',
   'v2.0.0' ]
   */
-console.log(sorted);
+//console.log(sorted);
+
+const octokit = new Octokit({
+ auth: process.env.GREN_GITHUB_TOKEN
+})
+const owner = 'plyo'
+const repo = 'plyo.web'
+
+getReleasesToDiff(octokit, owner, repo).
+then(function({from, to}) {
+  console.log({from, to});
+})
+
+async function getReleasesToDiff(octokit, owner, repo) {
+  const res = await getTwoLatestReleases(octokit, owner, repo)
+  const latest = res[0]
+  const prelatest = res[1]
+  const from = calculateReleaseToDiffWith(latest, prelatest)
+  const to = latest
+  return {from, to}
+}
+
+function calculateReleaseToDiffWith(latest, prelatest) {
+  const latestVer = semver.coerce(latest)
+  const prelatestVer = semver.coerce(prelatest)
+  if (latestVer.major != prelatestVer.major) {
+    // major release
+    return `v${prelatestVer.major}.${prelatestVer.minor}.0`
+  } else if (latestVer.minor != prelatestVer.minor) {
+    // minor release
+    return `v${prelatestVer.major}.${prelatestVer.minor}.0`
+  } else if (latestVer.patch != prelatestVer.patch) {
+    // patch release or prerelease
+    return `v${prelatestVer.major}.${prelatestVer.minor}.${prelatestVer.patch}`
+  }
+}
+
+async function getTwoLatestReleases(octokit, owner, repo) {
+  const res = await octokit.repos.listReleases({
+    owner,
+    repo
+  })
+  const rs = res.data.map(function (r) {return r.name})
+  if (rs.length < 2) {
+    console.log('Found less than 2 releases.')
+    process.exit(1);
+  }
+  semver.rsort(rs)
+  const latest = rs[0]
+  const prelatest = rs[1]
+  return [latest, prelatest]
+}
