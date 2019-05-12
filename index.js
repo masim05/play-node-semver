35584d90902807aacf857548b12dbc62b5a08036
Max Ryndin <rgmasim@gmail.com>
2019-05-12 11:38:35 +0300

    Add a comment.

--- index.js
+++ index.js
@@ -13,4 +13,17 @@ const list  = [
   'v1.1.0',
 ]
 const sorted = semver.sort(list)
+/*
+ * should print
+  [ 'v1.0.0',
+  'v1.0.1-alpha.0',
+  'v1.0.1-alpha.1',
+  'v1.0.1-beta.0',
+  'v1.0.1-rc.0',
+  'v1.0.1',
+  'v1.0.2',
+  'v1.1.0',
+  'v1.1.1',
+  'v2.0.0' ]
+  */
 console.log(sorted);
