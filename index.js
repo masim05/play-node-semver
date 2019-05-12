a9725d5bb1f18bcf7d7b9320ec11ec10e412a62b
Max Ryndin <rgmasim@gmail.com>
2019-05-12 11:37:17 +0300

    Add use case 1.

---
+++ index.js
@@ -0,0 +1,16 @@
+const semver = require('semver')
+
+const list  = [
+  'v1.0.0',
+  'v1.0.1-beta.0',
+  'v1.0.1-alpha.1',
+  'v1.0.1',
+  'v1.0.1-alpha.0',
+  'v1.0.2',
+  'v1.1.1',
+  'v1.0.1-rc.0',
+  'v2.0.0',
+  'v1.1.0',
+]
+const sorted = semver.sort(list)
+console.log(sorted);
